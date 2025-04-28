7b11bc26f013818c91b58c4f4ff97a637f58064a
Lucy <91936727+sgusde2105@users.noreply.github.com>
2025-04-28 21:42:06 +0900

    script.js

--- script.js
+++ script.js
@@ -1,63 +1,21 @@
-const API_KEY = "196159874954b852211c889a8e00cd52";
+// 現在の時間を取得
+const hour = new Date().getHours();
 
-fetch('https://ipapi.co/json/')
-  .then(response => response.json())
-  .then(locationData => {
-    console.log("位置情報取得成功:", locationData);
-    
-    const { latitude, longitude } = locationData;
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=ja&units=metric`;
+// プレイリストURLをここに設定
+const playlists = {
+  morning: "https://open.spotify.com/playlist/5eZRF45oQTOTKxc6LlrdCC", // 朝6〜10時
+  evening: "https://open.spotify.com/playlist/2zQmS4CcnQRlWh6mY5ph23", // 夕方16〜18時
+  night: "ttps://open.spotify.com/playlist/0rwum0FuoiCw54BVeHdJdO",   // 夜18〜20時
+  default: "https://open.spotify.com/playlist/7Ma3BudKw1eVehoTYh5y3F"   // それ以外
+};
 
-    return fetch(weatherUrl);
-  })
-  .then(response => response.json())
-  .then(weatherData => {
-    console.log("天気情報取得成功:", weatherData);
-    
-    const weatherId = weatherData.weather[0].id;
-    const hour = new Date().getHours();
-
-    console.log("天気ID:", weatherId, "時間:", hour);
-
-    const playlists = {
-      rain_morning: "https://open.spotify.com/playlist/0QtiVaZlEkewSrYbKLQTCX",
-      rain_night: "https://open.spotify.com/playlist/50kn9XQb3B6Tbn0ErdskBo",
-      clear_morning: "https://open.spotify.com/playlist/5eZRF45oQTOTKxc6LlrdCC",
-      clear_night: "https://open.spotify.com/playlist/2zQmS4CcnQRlWh6mY5ph23",
-      default: "https://open.spotify.com/playlist/7Ma3BudKw1eVehoTYh5y3F"
-    };
-
-    let isRain = (weatherId >= 500 && weatherId < 600);
-    let isClear = (weatherId === 800);
-
-    if (isRain) {
-      if (hour >= 6 && hour < 10) {
-        console.log("雨の朝に該当！");
-        window.location.href = playlists.rain_morning;
-      } else if (hour >= 16 && hour < 20) {
-        console.log("雨の夜に該当！");
-        window.location.href = playlists.rain_night;
-      } else {
-        console.log("雨だけど時間外なのでデフォルトへ！");
-        window.location.href = playlists.default;
-      }
-    } else if (isClear) {
-      if (hour >= 6 && hour < 10) {
-        console.log("晴れの朝に該当！");
-        window.location.href = playlists.clear_morning;
-      } else if (hour >= 16 && hour < 20) {
-        console.log("晴れの夜に該当！");
-        window.location.href = playlists.clear_night;
-      } else {
-        console.log("晴れだけど時間外なのでデフォルトへ！");
-        window.location.href = playlists.default;
-      }
-    } else {
-      console.log("天気が雨でも晴れでもないのでデフォルトへ！");
-      window.location.href = playlists.default;
-    }
-  })
-  .catch(error => {
-    console.error('エラー発生:', error);
-    window.location.href = "https://open.spotify.com/playlist/7Ma3BudKw1eVehoTYh5y3F";
-  });
+// 条件分岐してリダイレクト
+if (hour >= 6 && hour < 12) {
+  window.location.href = playlists.morning;
+} else if (hour >= 16 && hour < 19) {
+  window.location.href = playlists.evening;
+} else if (hour >= 19 && hour < 23) {
+  window.location.href = playlists.night;
+} else {
+  window.location.href = playlists.default;
+}
